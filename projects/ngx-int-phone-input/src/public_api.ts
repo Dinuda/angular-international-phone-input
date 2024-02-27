e9f1c2c993968bfec38de21713e3de40768ef5a2
Dinuda <ydinuda@gmail.com>
2024-02-27 19:25:14 +0530

    feat: main

---
+++ projects/ngx-int-phone-input/src/public_api.ts
@@ -0,0 +1,11 @@
+/*
+ * Public API Surface of ngx-int-phone-input
+ */
+
+export * from './lib/ngx-int-phone-input.component';
+export * from './lib/directives/native-element-injector.directive';
+export * from './lib/ngx-int-phone-input.module';
+export * from './lib/enums/country-iso.enum';
+export * from './lib/enums/search-country-field.enum';
+export * from './lib/enums/phone-number-format.enum';
+export * from './lib/interfaces/change-data';
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import {
+	NgxIntlTelInputModule
+} from 'projects/ngx-int-phone-input/src/lib/ngx-int-phone-input.module';
+
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		ReactiveFormsModule,
+		NgxIntlTelInputModule,
+		BrowserAnimationsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent],
+})
+export class AppModule {}
